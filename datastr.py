4140128e508ccafffafeb520692f544766fb439b
manomay <pachaderahul1@gmail.com>
2024-01-16 18:17:13 +0530

    First Commit

--- datastr.py
+++ datastr.py
@@ -1,6 +1,8 @@
+l1 = []
 for i in range(1,51):
   if i % 5 == 0:
+    l1.append(i)
     print("The Number is Divisible by 5!!! ")
   else:
-    print("The Number is Not Divisible!!!")
-
+    print("The Number is Not Divisible")
+print(f"The {i} is Divisible by 5!!! {l1}")
